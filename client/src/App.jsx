b37da25d67670ea25d6f6eada557d8609d653ab4
Aban Muhammed C P <abancpchengani@gmail.com>
2025-04-20 10:54:32 +0530

    client work started

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-full h-screen bg-gray-200 flex gap-2 p-3' >
+      <div className='w-[14rem] h-full flex flex-col justify-start items-center gap-2 p-2'>
+        <h1 className='text-3xl font-black font-[Playwrite_RO]'>Omodels</h1>
+      </div>
+      <div className='w-full h-full bg-white rounded-lg '></div>
+    </div>
+  )
+}
+
+export default App
